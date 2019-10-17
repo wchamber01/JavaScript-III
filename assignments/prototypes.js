ecea8daf605704b43bcb52ea9088635d1a1cd0a5
wchamber01 <52251104+wchamber01@users.noreply.github.com>
2019-10-17 18:26:19 -0400

    finished 'this' section

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -32,7 +32,7 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
- 
+
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -41,7 +41,7 @@
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,7 +102,7 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,53 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding: The global space where "this" defaults to when not in strict mode.
+* 2. Implicit Binding: Where "this" refers to what's found left of the 'dot'.
+* 3. New Binding: "This points to new object that has been created."
+* 4. Explicit Binding: Is used with '.call', '.apply' or '.bind'
 *
 * write out a code example of each explanation above
 */
 
-// Principle 1
+window.age = 10
 
-// code example for Window Binding
+function dogAge () {
+    console.log(`Hickory is ${this.age} years old.`)
+}
 
 // Principle 2
-
 // code example for Implicit Binding
 
-// Principle 3
+const user = {
+    name: 'William',
+    age: 35,
+    greet: function() {
+        alert(`Hello, my name is ${this.name}`)
+    }
+}
+user.greet();
 
+// Principle 3
 // code example for New Binding
 
-// Principle 4
+function pet(name, age){
+    this.name = name;
+    this.age = age;
+}
 
+const dog =  new pet('Hickory', 10);
+console.log(dog.age)
+
+// Principle 4
 // code example for Explicit Binding
+
+function dogGreet () {
+    alert(`Hello, my dog's name is ${this.name}.`)
+}
+
+const myDog = {
+    name: 'Hickory',
+    age: 10,
+}
+
+dogGreet.call(myDog)
